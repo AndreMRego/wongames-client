a6259c866e461d7e8e48a54e5302ed1bf0dd1b0e
Andre Rego <andremregodev@gmail.com>
2021-02-06 20:02:07 -0300

    add Menu Component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Menu from '.';
+
+export default {
+  title: 'Menu',
+  component: Menu,
+} as Meta;
+
+export const Mobile: Story = (args) => <Menu {...args} />;
+
+Mobile.parameters = {
+  layout: 'fullscreen',
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  backgrounds: {
+    default: 'dark',
+  },
+};
