458f5e54f03e7c3b4a3b3bc3ab511081e2f8a9a8
Andre Rego <andremregodev@gmail.com>
2021-02-09 21:22:46 -0300

    add Banner and Ribbon Components

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,54 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Banner from '.';
+
+const mockProps = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death',
+};
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...mockProps} />);
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('img', { name: /Defy death/i }),
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...mockProps}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />,
+    );
+
+    const ribbon = screen.getByText(/My Ribbon/i);
+
+    expect(ribbon).toBeInTheDocument();
+
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' });
+
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem',
+    });
+  });
+});
