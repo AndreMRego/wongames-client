726a061a5387e500b461db2f694abd99c1b9fedc
Andre Rego <andremregodev@gmail.com>
2021-02-16 16:42:44 -0300

    add mocks and Home Page

---
+++ src/components/GameCard/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: '/img/red-dead-img.jpg',
+  price: 'R$ 235,00',
+  promotionalPrice: 'R$ 210,00',
+};
--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,17 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import GameCard, { GameCardProps } from '.';
+import item from './mock';
 
 export default {
   title: 'GameCard',
   component: GameCard,
-  args: {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: '/img/red-dead-img.jpg',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 210,00',
-  },
+  args: item,
   argTypes: {
     onFav: {
       action: 'clicked',
