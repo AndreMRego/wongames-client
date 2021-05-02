f893b44ee63692f1267fd7fdf97c21a71916b46c
Andre Rego <andremregodev@gmail.com>
2021-05-02 15:46:01 -0300

    add Gallery Component

---
+++ src/components/Gallery/test.tsx
@@ -0,0 +1,96 @@
+import 'match-media-mock';
+import { fireEvent, screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Gallery from '.';
+import mockItems from './mock';
+
+describe('<Gallery />', () => {
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i }),
+    ).toHaveAttribute('src', mockItems[0].src);
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i }),
+    ).toHaveAttribute('src', mockItems[1].src);
+  });
+
+  it('should handle the open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    //seleciona o modal
+    const modal = screen.getByLabelText('modal');
+
+    //verificar se está escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' });
+
+    const thumb = screen.getByRole('button', {
+      name: /Thumb - Gallery Image 1/i,
+    });
+
+    //clica na imagem para abrir o modal
+    fireEvent.click(thumb);
+    expect(modal.getAttribute('aria-hidden')).toBe('false');
+    expect(modal).toHaveStyle({ opacity: 1 });
+  });
+
+  it('should open modal with selected image', async () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    const thumb = screen.getByRole('button', {
+      name: /Thumb - Gallery Image 2/i,
+    });
+
+    //clica na imagem para abrir o modal
+    fireEvent.click(thumb);
+
+    const img = await screen.findByRole('img', { name: /Gallery Image 2/i });
+    expect(img.parentElement?.parentElement).toHaveClass('slick-active');
+  });
+
+  it('should handle the close modal when overlay or button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    //seleciona o modal
+    const modal = screen.getByLabelText('modal');
+
+    const thumb = screen.getByRole('button', {
+      name: /Thumb - Gallery Image 1/i,
+    });
+
+    //clica na imagem para abrir o modal
+    fireEvent.click(thumb);
+
+    //clica na imagem para fechar o modal
+    fireEvent.click(screen.getByLabelText(/close modal/i));
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0 });
+  });
+
+  it('should handle the close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={mockItems.slice(0, 2)} />,
+    );
+
+    //seleciona o modal
+    const modal = screen.getByLabelText('modal');
+
+    const thumb = screen.getByRole('button', {
+      name: /Thumb - Gallery Image 1/i,
+    });
+
+    //clica na imagem para abrir o modal
+    fireEvent.click(thumb);
+
+    //clica na imagem para fechar o modal
+    fireEvent.keyUp(container, { key: 'Escape' });
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0 });
+  });
+});
