9103278ff950ae1c530dc7e05ad64c17e1c82643
Andre Rego <andremregodev@gmail.com>
2021-05-03 20:16:14 -0300

    add Game Page

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -9,7 +9,7 @@ const props: GameDetailsProps = {
   releaseDate: '2020-11-21T23:00:00',
   rating: 'BR0',
   genres: ['Role-playing', 'Narrative'],
-  publisher: '2K',
+  publisher: 'Walktrough',
 };
 
 describe('<GameDetails />', () => {
@@ -60,6 +60,18 @@ describe('<GameDetails />', () => {
     renderWithTheme(<GameDetails {...props} />);
 
     expect(screen.getByText(/free/i)).toBeInTheDocument();
+  });
+
+  it('should render the developer', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText(/Different Tales/i)).toBeInTheDocument();
+  });
+
+  it('should render the publisher', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText(/walktrough/i)).toBeInTheDocument();
   });
 
   it('should render 18+ rating when BR18', () => {
