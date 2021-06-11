527e932fd81f312c5b3985013b65a8dac7af2bea
Andre Rego <andremregodev@gmail.com>
2021-06-11 12:01:58 -0300

    Add type generation to our GraphQL queries

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -11,7 +11,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject>;
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: 'http://192.168.171.61:1337/graphql' }),
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
     cache: new InMemoryCache(),
   });
 }
