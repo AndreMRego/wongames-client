028230fbaa607e0ede58043c4163caa4a83dcf21
Andre Rego <andremregodev@gmail.com>
2021-07-12 22:56:04 -0300

    add Empty when there are no games to the filter

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -94,4 +94,16 @@ describe('<Games />', () => {
       },
     });
   });
+
+  it('should render empty when no games found', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>,
+    );
+
+    expect(
+      await screen.findByText(/We didn't find any games with this filter/i),
+    ).toBeInTheDocument();
+  });
 });
