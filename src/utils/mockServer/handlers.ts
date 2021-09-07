ea0d7f7019dd2cd17617da5f9abd11e0f8b34b38
Andre Rego <andremregodev@gmail.com>
2021-09-07 20:06:04 -0300

    Add handler to forgot-password

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,1 +1,41 @@
-export const handlers = [];
+import { rest } from 'msw';
+
+type LoginReqBody = {
+  email: string;
+};
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body;
+
+      // testar quando der error
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist',
+                  },
+                ],
+              },
+            ],
+          }),
+        );
+      }
+      // testar quando der sucesso
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true,
+        }),
+      );
+    },
+  ),
+];
