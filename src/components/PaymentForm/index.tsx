6a27bd3e963f213fed04c1330326cff316c98e4d
Andre Rego <andremregodev@gmail.com>
2022-02-06 16:55:06 -0300

    Start useEffect implementation to create paymentIntent

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined';
 import { CardElement } from '@stripe/react-stripe-js';
 import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
 import Button from 'components/Button';
 import Heading from 'components/Heading';
+import { useCart } from 'hooks/use-cart';
 import * as S from './styles';
 
 const PaymentForm = () => {
+  const { items } = useCart();
   const [error, setError] = useState<string | null>(null);
   const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const [freeGames, setFreeGames] = useState(false);
 
+  useEffect(() => {
+    if (items.length) {
+      // bater na API /orders/create-payment-intent
+      // enviar items do carrinho
+      // se receber freeGames: true, setar setFreeGames(true)
+      //fazer fluxo de jogo gratuito
+      //se receber um erro, setar setError(error)
+      //senão o paymentIntent foi válido
+      //setar setClientSecret
+    }
+  }, [items]);
   const handleChange = async (event: StripeCardElementChangeEvent) => {
     setDisabled(event.empty);
     setError(event.error ? event.error.message : '');
