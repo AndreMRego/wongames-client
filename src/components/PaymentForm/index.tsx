549778c8e635fff5d6ee68e12c7c0f739acc7722
Andre Rego <andremregodev@gmail.com>
2022-02-06 16:33:50 -0300

    Add errors handler from Stripe

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined';
 import { CardElement } from '@stripe/react-stripe-js';
-import { ShoppingCart } from '@styled-icons/material-outlined';
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
 import Button from 'components/Button';
 import Heading from 'components/Heading';
 import * as S from './styles';
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setError(event.error ? event.error.message : '');
+  };
+
   return (
     <S.Wrapper>
       <S.Body>
@@ -16,7 +24,14 @@ const PaymentForm = () => {
           options={{
             hidePostalCode: true,
           }}
+          onChange={handleChange}
         />
+        {error && (
+          <S.Error>
+            <ErrorOutline size={20} />
+            {error}
+          </S.Error>
+        )}
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
