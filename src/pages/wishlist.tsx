d80bd4c1e97d7bb6bb2bbb3485314c7ad065be5f
Andre Rego <andremregodev@gmail.com>
2022-02-06 00:24:48 -0300

    Add Wishlist Page

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -8,13 +8,26 @@ import { QUERY_RECOMMENDED } from 'graphql/queries/recommended';
 import { initializeApollo } from 'utils/apollo';
 import { gamesMapper, highlightMapper } from 'utils/mappers';
 import protectedRoutes from 'utils/protected-routes';
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist';
+import {
+  QueryWishlist,
+  QueryWishlistVariables,
+} from 'graphql/generated/QueryWishlist';
 
 const WishlistPage = (props: WishlistTemplateProps) => <WishList {...props} />;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context);
+  const apolloClient = initializeApollo(null, session);
 
-  const apolloClient = initializeApollo();
+  if (!session) return {};
+
+  await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: session?.user?.email as string,
+    },
+  });
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED,
@@ -22,12 +35,13 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   return {
     props: {
+      session,
+      initialApolloState: apolloClient.cache.extract(),
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
         data.recommended?.section?.highlight,
       ),
-      session,
     },
   };
 }
--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -4,54 +4,65 @@ import Heading from 'components/Heading';
 import { Container } from 'components/Container';
 import GameCard, { GameCardProps } from 'components/GameCard';
 import { HighlightProps } from 'components/Highlight';
+import Loader from 'components/Loader';
 
 import Showcase from 'components/Showcase';
 import { Grid } from 'components/Grid';
 import { Divider } from 'components/Divider';
 import Empty from 'components/Empty';
+import { useWishlist } from 'hooks/use-wishlist';
+
+import * as S from './styles';
 
 export type WishlistTemplateProps = {
-  games?: GameCardProps[];
   recommendedTitle: string;
   recommendedGames: GameCardProps[];
   recommendedHighlight: HighlightProps;
 };
 
 const Wishlist = ({
-  games = [],
   recommendedTitle,
   recommendedGames,
   recommendedHighlight,
-}: WishlistTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Wishlist
-      </Heading>
+}: WishlistTemplateProps) => {
+  const { items, loading } = useWishlist();
 
-      {games.length ? (
-        <Grid>
-          {games?.map((game, index) => (
-            <GameCard key={`wishlist-${index}`} {...game} />
-          ))}
-        </Grid>
-      ) : (
-        <Empty
-          title="Your wishlist is empty"
-          description="Games added to your wishlist will appear here"
-          hasLink
-        />
-      )}
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Wishlist
+        </Heading>
 
-      <Divider />
-    </Container>
+        {loading && (
+          <S.Loading>
+            <Loader />
+          </S.Loading>
+        )}
+        {items.length ? (
+          <Grid>
+            {items?.map((game, index) => (
+              <GameCard key={`wishlist-${index}`} {...game} />
+            ))}
+          </Grid>
+        ) : (
+          <Empty
+            title="Your wishlist is empty"
+            description="Games added to your wishlist will appear here"
+            hasLink
+          />
+        )}
 
-    <Showcase
-      title={recommendedTitle}
-      games={recommendedGames}
-      highlight={recommendedHighlight}
-    />
-  </Base>
-);
+        <Divider />
+      </Container>
+
+      <Showcase
+        title={recommendedTitle}
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  );
+};
 
 export default Wishlist;
