39ed733253f475803bc69e8d29970144aebef0b2
Andre Rego <andremregodev@gmail.com>
2022-02-05 23:22:43 -0300

    Create WishlistButton

---
+++ src/components/WishlistButton/test.tsx
@@ -0,0 +1,108 @@
+import { render, screen } from 'utils/test-utils';
+import { WishlistContextDefaultValues } from 'hooks/use-wishlist';
+
+import WishlistButton from '.';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useSession = jest.spyOn(require('next-auth/client'), 'useSession');
+const session = {
+  jwt: '123',
+  user: {
+    email: 'lorem@ipsum.com',
+  },
+};
+useSession.mockImplementation(() => [session]);
+
+describe('<WishlistButton />', () => {
+  it('should render a button to add to wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false,
+    };
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps });
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button to remove from wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps });
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button with add to wishlist text', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false,
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    expect(screen.getByText(/add to wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button with remove from wishlist text', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps });
+
+    expect(screen.getByText(/remove from wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render a button with small size', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps });
+
+    expect(
+      screen.getByLabelText(/remove from wishlist/i).parentElement,
+    ).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem',
+    });
+  });
+
+  it('should render a button with medium size', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" size="medium" />, { wishlistProviderProps });
+
+    expect(
+      screen.getByLabelText(/remove from wishlist/i).parentElement,
+    ).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem',
+    });
+  });
+
+  it('should not render if not logged', () => {
+    useSession.mockImplementationOnce(() => [null]);
+
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+    };
+
+    render(<WishlistButton id="1" size="medium" />, { wishlistProviderProps });
+
+    expect(
+      screen.queryByLabelText(/remove from wishlist/i),
+    ).not.toBeInTheDocument();
+  });
+});
