07abdb78015aa4bbb2b72bda02e39e11feba543e
Andre Rego <idd_arego@uolinc.com>
2022-06-14 18:38:46 -0300

    Add NextSEO default

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.wongames.andrerego.com.br',
+    site_name: 'Won Games',
+    title: 'Won Games',
+  },
+  twitter: {
+    handle: '@user',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
