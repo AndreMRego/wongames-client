86f8ac94834d02e40932b053513a938ab450297f
Andre Rego <idd_arego@uolinc.com>
2022-06-15 21:53:49 -0300

    Using click and wait to animation on banner

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -9,6 +9,18 @@ describe('Home Page', () => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i })
       cy.findByRole('link', { name: /buy now/i })
     })
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /horizon zero dawn/i })
+      cy.findAllByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /huge promotion!/i })
+      cy.findByRole('link', { name: /browse all games/i })
   });
 
 });
